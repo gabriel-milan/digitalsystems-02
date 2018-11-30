cf46d8b5e159eac4acefac990f166cbb7efc5600
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2018-11-30 16:46:58 -0200

    Added keyboard test code w/ key parser

---
+++ key_parser.vhd
@@ -0,0 +1,33 @@
+-- Key code parser
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Entity
+entity key_parser is
+	Port (
+		key_code : in  STD_LOGIC_VECTOR(7 downto 0);
+		binary_out : out  STD_LOGIC_VECTOR (3 downto 0)
+	);
+end key_parser;
+
+-- Architecture
+architecture Behavioral of key_parser is
+
+-- Behavioral
+begin
+
+   with key_code select
+      binary_out <=
+         "0000" when "01000101",  -- 0
+         "0001" when "00010110",  -- 1
+         "0010" when "00011110",  -- 2
+         "0011" when "00100110",  -- 3
+         "0100" when "00100101",  -- 4
+         "0101" when "00101110",  -- 5
+         "0110" when "00110110",  -- 6
+         "0111" when "00111101",  -- 7
+         "1000" when "00111110",  -- 8
+         "1001" when "01000110",  -- 9
+         "1111" when others;      -- -1
+
+end Behavioral;
---
+++ test_keyboard.vhd
@@ -0,0 +1,66 @@
+-- Código para teste do teclado PS/2
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Entidade
+entity test_keyboard is
+   Port (
+      clk, reset: in  std_logic;
+      ps2d, ps2c: in  std_logic;
+		leds : out STD_LOGIC_VECTOR(7 downto 0)		-- LEDs para indicar tecla pressionada
+	);
+end test_keyboard;
+
+-- Arquitetura
+architecture Behavioral of test_keyboard is
+
+-- Componente do PS/2 RX
+component ps2_rx
+	port (
+      clk, reset: in  std_logic;
+      ps2d, ps2c: in  std_logic;
+      rx_en: in std_logic;
+      rx_done_tick: out  std_logic;
+      dout: out std_logic_vector(7 downto 0)
+	);
+end component;
+
+-- Componente do parser de tecla
+component key_parser
+	port (
+		key_code : in  STD_LOGIC_VECTOR(7 downto 0);
+		binary_out : out  STD_LOGIC_VECTOR (3 downto 0)
+	);
+end component;
+
+-- Signals
+signal rx_done : STD_LOGIC;
+signal key_out : STD_LOGIC_VECTOR(7 downto 0);
+signal binary_out : STD_LOGIC_VECTOR (3 downto 0);
+
+-- Comportamental
+begin
+
+	-- Port mapping
+	U0: ps2_rx port map (clk, reset, ps2d, ps2c, '1', rx_done, key_out);
+	U1: key_parser port map (key_out, binary_out);
+	
+	-- Process
+	process (clk, reset, rx_done)
+	begin
+	
+		-- Clock event
+		if (clk'event and clk = '1') then
+		
+			-- Se o RX tiver recebido o dado
+			if (rx_done = '1') then
+			
+				leds <= "0000" & binary_out;
+			
+			end if;
+		
+		end if;
+	
+	end process;
+
+end Behavioral;
